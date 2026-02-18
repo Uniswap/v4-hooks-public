6f3c2c8c2502dc4362cbc1035ead0d66b745ea37
ericneil-sanc <eric.sanchirico@uniswap.org>
2026-02-18 17:46:37 -0500

    bump interfaces to ^0.8.29

--- src/aggregator-hooks/BaseAggregatorHook.sol
+++ src/aggregator-hooks/BaseAggregatorHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.29;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
--- src/aggregator-hooks/implementations/StableSwap/interfaces/ICurveFactory.sol
+++ src/aggregator-hooks/implementations/StableSwap/interfaces/ICurveFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.29;
 
 /// @title ICurveFactory
 /// @notice Interface for Curve Metapool Factory
